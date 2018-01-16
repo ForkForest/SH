df2948cc221735b1eae903ec4254cab1272a43a8
Sunlewuyou <258657759@qq.com>
2018-01-16 17:42:29 +0800

    Update autossh.sh

--- autossh.sh
+++ autossh.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget -c -O /tmp/autossh-1.4e.tgz --no-check-certificate "https://raw.githubusercontent.com/sunlewuyou/SH/master/autossh-1.4e.tgz
+wget -c -O /tmp/autossh-1.4e.tgz --no-check-certificate "https://raw.githubusercontent.com/sunlewuyou/SH/master/autossh-1.4e.tgz"
 tar -xvf autossh-1.4e.tgz
 cd autossh-1.4e
 ./configure
